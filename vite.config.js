e337b43ba2fedde8fc52eeed91ac90ea34e8bf66
Mariam Aljizawi <mariam_97@live.se>
2025-06-02 04:15:26 +0200

    Add: Configuration to be able to use Vite correctly.

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import { fileURLToPath } from 'url';
+import path, { dirname, resolve } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default defineConfig({
+  root: 'src',
+  build: {
+    outDir: '../dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html'),
+        login: resolve(__dirname, 'src/login.html'),
+        register: resolve(__dirname, 'src/register.html'),
+      },
+    },
+  },
+});
